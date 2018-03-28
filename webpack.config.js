a7cc3995617b1f73bc15fe722e137f087e8fd2f6
cohki0305 <cohki0305@gmail.com>
2018-03-28 22:45:14 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './src/index.html', to: "index.html" }
+    ])
+  ],
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    rules: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
